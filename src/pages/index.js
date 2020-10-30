eb969f3cfb8ef52b420b999f43f539b39e40f505
luxumbra <me@luxumbra.dev>
2020-10-30 01:30:17 +0000

    Switched out the logo as @peth-yursick had in his update and added a couple of responsive tweaks.

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,12 +98,14 @@ const sectionHeading = {
   fontFamily: '"IBM Plex Mono"',
   fontSize: '24px',
   color: '#a5b9f6',
-  marginBottom: '13px',
 };
 const panelTitleStyle = {
   ...sectionHeading,
   color: 'white',
   marginBottom: '3px',
+  flex: '0 0 100%',
+  width: '100%',
+  textAlign: 'left',
 };
 const textStyle = {
   fontSize: '14px',
@@ -164,14 +166,19 @@ function Home() {
   return (
     <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <div className={classnames('wrapper', wikipath)}>
-        <div className={classnames('inner-wrapper')} style={wrapperStyle}>
+        <div
+          className={classnames('inner-wrapper', styles.innerWrapper)}
+          style={wrapperStyle}
+        >
           <header className={classnames(styles.heroBanner)}>
             <div className='container'>
               <img
                 alt='MetaGame Wiki Logo'
                 className={styles.wikiLogo}
                 width='555'
-                src={useBaseUrl('img/wiki-logo.png')}
+                src={
+                  'https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fe93a37b7-2a48-421c-80b7-3079eca8beb7_2048x881.png'
+                }
               />
               <p className={styles.heroSubtitle}>
                 <em>A Massive Online Coordination Game</em>
@@ -186,119 +193,117 @@ function Home() {
                 justifyContent: 'center',
               }}
             >
-              {/* <p>
-                      <img
-                        src='https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fe93a37b7-2a48-421c-80b7-3079eca8beb7_2048x881.png'
-                        alt=''
-                      />
-                      <br />
-                      <em>A Massive Online Coordination Game</em>
-                    </p> */}
-              <h2 id='abstract'>Abstract</h2>
-              <blockquote>
+              <div className={'col'}>
+                <h2 id='abstract'>Abstract</h2>
+                <blockquote>
+                  <p>
+                    <em>
+                      “Metagame is any approach to a game that transcends or
+                      operates outside of the prescribed rules of the game, uses
+                      external factors to affect the game, or goes beyond the
+                      supposed limits or environment set by the game.”
+                    </em>{' '}
+                    - From Wiki
+                  </p>
+                </blockquote>
                 <p>
-                  <em>
-                    “Metagame is any approach to a game that transcends or
-                    operates outside of the prescribed rules of the game, uses
-                    external factors to affect the game, or goes beyond the
-                    supposed limits or environment set by the game.”
-                  </em>{' '}
-                  - From Wiki
+                  Players of MetaGame are on a quest to change the way people
+                  coordinate around solving problems &amp; creating value.
                 </p>
-              </blockquote>
-              <p>
-                Players of MetaGame are on a quest to change the way people
-                coordinate around solving problems &amp; creating value.
-              </p>
-              <p>
-                The quest stretches from fixing the onboarding to the Ethereum
-                space
-                <br />
-                by symmetrifying information &amp; composing the basic
-                socio-economic infrastructure, to grappling with actual
-                problems.
-              </p>
-              <ul>
-                <li>
-                  You know, the opposite of the first crypto-world problems.
-                </li>
-              </ul>
-              <p>But, it all starts with Seeds…</p>
-              <h2 id='seed-phase'>Seed Phase</h2>
-              <p>
-                <img
-                  src='https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Ffa305be3-279a-440a-9ba2-cfa2d49281c4_952x186.png'
-                  alt=''
-                />
-              </p>
-              <p>
-                The main goal of Seed phase is to build a hub for anyone
-                interested in decentralized
-                <br />
-                organizations &amp; applications.
-                <br />
-                Guides &amp; portals to anything one might need to build them
-                &amp; a community of people to support them.
-              </p>
-              <p>
-                We’re building a <em>Decentralized Factory</em> for baking the
-                building blocks we are going to use to compose the bigger
-                picture.
-              </p>
-              <h2 id='the-bigger-picture'>The Bigger Picture</h2>
-              <p>
-                <img
-                  src='https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fbe6c92b4-6ead-4a60-acf0-db9cc0251b9c_1310x265.png'
-                  alt=''
-                />
-              </p>
-              <p>
-                The long term goal of MetaGame is to build an alternative
-                society; a global society focused around solving problem - not
-                squeezing the world for profit.
-              </p>
-              <p>We are building the future we want to live in:</p>
-              <ul>
-                <li>
-                  A more collaborative, transparent &amp; decentralized future.
-                </li>
-                <li>
-                  A future in which human progress isn’t accelerating its
-                  destruction.
-                </li>
-              </ul>
-              <p>Read more about the Phases of MetaGame?</p>
+                <p>
+                  The quest stretches from fixing the onboarding to the Ethereum
+                  space
+                  <br />
+                  by symmetrifying information &amp; composing the basic
+                  socio-economic infrastructure, to grappling with actual
+                  problems.
+                </p>
+                <ul>
+                  <li>
+                    You know, the opposite of the first crypto-world problems.
+                  </li>
+                </ul>
+                <p>But, it all starts with Seeds…</p>
+                <h2 id='seed-phase'>Seed Phase</h2>
+                <p>
+                  <img
+                    src='https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Ffa305be3-279a-440a-9ba2-cfa2d49281c4_952x186.png'
+                    alt=''
+                  />
+                </p>
+                <p>
+                  The main goal of Seed phase is to build a hub for anyone
+                  interested in decentralized
+                  <br />
+                  organizations &amp; applications.
+                  <br />
+                  Guides &amp; portals to anything one might need to build them
+                  &amp; a community of people to support them.
+                </p>
+                <p>
+                  We’re building a <em>Decentralized Factory</em> for baking the
+                  building blocks we are going to use to compose the bigger
+                  picture.
+                </p>
+                <h2 id='the-bigger-picture'>The Bigger Picture</h2>
+                <p>
+                  <img
+                    src='https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fbe6c92b4-6ead-4a60-acf0-db9cc0251b9c_1310x265.png'
+                    alt=''
+                  />
+                </p>
+                <p>
+                  The long term goal of MetaGame is to build an alternative
+                  society; a global society focused around solving problem - not
+                  squeezing the world for profit.
+                </p>
+                <p>We are building the future we want to live in:</p>
+                <ul>
+                  <li>
+                    A more collaborative, transparent &amp; decentralized
+                    future.
+                  </li>
+                  <li>
+                    A future in which human progress isn’t accelerating its
+                    destruction.
+                  </li>
+                </ul>
+                <p>Read more about the Phases of MetaGame?</p>
+              </div>
             </div>
 
             {features && features.length && (
               <section className={styles.features}>
                 <div className='container'>
-                  <div
-                    className='row'
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <h2 style={sectionHeading}>So, who is it for?</h2>
-                    {features &&
-                      features.map((props, i, idx) => (
-                        <>
-                          {features.length === i + 1 && (
-                            <h2 style={sectionHeading}>Who is it NOT for?</h2>
-                          )}
-                          <Panel
-                            key={idx}
-                            lastPlayerPanel={
-                              i === features.length - 2 ? 'last-item' : ''
-                            }
-                            lamerPanel={
-                              i === features.length - 1 ? 'lamer-panel' : ''
-                            }
-                            {...props}
-                          />
-                        </>
-                      ))}
+                  <div className='row'>
+                    <div
+                      className={'col'}
+                      style={{
+                        display: 'flex',
+                        flexFlow: 'row wrap',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <h2 style={sectionHeading}>So, who is it for?</h2>
+                      {features &&
+                        features.map((props, i, idx) => (
+                          <>
+                            {features.length === i + 1 && (
+                              <h2 style={sectionHeading}>Who is it NOT for?</h2>
+                            )}
+                            <Panel
+                              key={idx}
+                              lastPlayerPanel={
+                                i === features.length - 2 ? 'last-item' : ''
+                              }
+                              lamerPanel={
+                                i === features.length - 1 ? 'lamer-panel' : ''
+                              }
+                              {...props}
+                            />
+                          </>
+                        ))}
+                    </div>
                   </div>
                 </div>
               </section>
