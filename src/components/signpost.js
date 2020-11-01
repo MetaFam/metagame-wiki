7be2006c691e1afd94569a8845aea6a040fbf2f2
luxumbra <me@luxumbra.dev>
2020-11-01 12:04:01 +0000

    removed unwanted text

--- src/components/signpost.js
+++ src/components/signpost.js
@@ -123,9 +123,6 @@ export function Signpost() {
         width='300'
         src='https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fe93a37b7-2a48-421c-80b7-3079eca8beb7_2048x881.png'
       />
-      <p>
-        There are many ways to go about in MetaGame, here are the main ones.
-      </p>
       <ul className={styles.signpost}>
         {directions &&
           directions.length > 0 &&
