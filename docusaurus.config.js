d4ee86dbc96d4c806bd44537a9b0357cf3f692cc
luxumbra <me@luxumbra.dev>
2021-01-22 01:30:51 +0000

    a more suitable domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "MetaGame Wiki",
   tagline: "Build the future you want to live in.",
-  url: "https://wiki.metagame.wtf",
+  url: "http://metawiki.luxumbra.dev/",
   baseUrl: "/",
   favicon: "img/mg-icon.png",
   organizationName: "MetaFam",
@@ -73,7 +73,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://metagame-wiki-iota.vercel.app/admin#/?",
+          editUrl: "http://metawiki.luxumbra.dev/admin#/?",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.scss"),
